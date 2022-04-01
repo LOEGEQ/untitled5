4f96c2832d16643732ddfef0c5172f90fe3dfb79
输了才怪 <https://gitee.com/qq2727696091/leam-java1.git>
2022-04-01 15:37:31 +0800

    2022 3 25

--- src/mian.java
+++ src/mian.java
@@ -1,10 +1,29 @@
 public class mian {
     public static void main(String[] args){
-        int year = 2001;
+          /*
+        //三目运算表达式例子：
+
+        //(boolean_expr)? true_statement : false_statement;
+        //条件表达式条件为真时表达式 ：条件为假时表达式
+             int a=-5;
+             System.out.println(a<=0?"A小于0":"A大于0");
+        */
+
+        double a=5;
+        double b=10;
+        double c=15;
+        double d=20;
+        double result =a+b*c/d+(a+d);
+        System.out.println(result);
+
+
+       /* int year = 2001;
         if ((year % 400 == 0) || (year %4 == 0 && year % 100 !=0) ){
             System.out.println("一定是闰年");
         }else{
             System.out.println("一定是平年");
+
         }
+        */
     }
 }
